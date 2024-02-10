d32b1de82ac2b7ba2a9762ce6d7536d1960883b5
Ann57 <spantonix@gmail.com>
2024-02-10 19:39:31 +0400

    2.2

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def index():
     return render_template('2.html')
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
